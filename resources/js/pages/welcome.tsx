9a08a1fcf9beb7eeb222149e0b680d7e7c14ec08
Muhammad.Rois <111403383+roismohammed@users.noreply.github.com>
2024-10-03 20:53:06 +0700

    update card

--- resources/js/pages/welcome.tsx
+++ resources/js/pages/welcome.tsx
@@ -16,79 +16,51 @@ const WelcomePage = ({ }) => {
 			{/* Halaman bagian page atas */}
 			{/* ariants={{hidden:{opacity:0},show:{opacity:1}}} */}
 			<div className="bg-slate-50 h-screen w-full">
-				<div className="flex p-4 md:p-16 gap-6">
+				<div className="flex flex-col md:flex-row p-4 md:p-16 gap-6">
 					<div className="w-full md:w-8/12">
-						<Card
-							className="rounded-xl border border-pink-200  shadow-none p-4 md:p-10"
-							style={{ background: "#FEF3F9" }}
-						>
+						<Card className="rounded-xl border border-pink-200 shadow-none p-4 md:p-10" style={{ background: "#FEF3F9" }}>
 							<CardContent>
 								<CardTitle>
-									<p
-										className="text-6xl font-bold"
-										style={{
-											color: "#E91C8A",
-											maxWidth: "500px",
-										}}
-									>
+									<p className="text-6xl font-bold" style={{ color: "#E91C8A", maxWidth: "500px" }}>
 										Template Design Undangan
 									</p>
 									<p className="text-sm mt-4 text-gray-600">
-										Temukan keindahan desain undangan
-										pernikahan impianmu.
+										Temukan keindahan desain undangan pernikahan impianmu.
 									</p>
 								</CardTitle>
-								<Button
-									className="rounded-full mt-4 md:mt-24 text-white flex gap-2"
-									style={{ background: "#EB578B" }}
-								>
-									Jelajahi Template{" "}
-									<IconArrowDown size={18} />
+								<Button className="rounded-full mt-4 md:mt-24 text-white flex gap-2" style={{ background: "#EB578B" }}>
+									Jelajahi Template <IconArrowDown size={18} />
 								</Button>
 							</CardContent>
 						</Card>
 
-						<div className="flex gap-6 mt-6">
-							<div className="w-full md:w-7/12">
-								<Card
-									className="rounded-xl border-pink-200  shadow-none p-10"
-									style={{ background: "#FEF3F9" }}
-								>
+						<div className="flex flex-col md:flex-row gap-6 mt-6">
+							<div className="w-full md:w-6/12">
+								<Card className="rounded-xl border-pink-200 shadow-none p-10" style={{ background: "#FEF3F9" }}>
 									<CardTitle>
 										<div className="flex gap-5">
 											<div className="bg-white p-3 rounded-xl items-center">
-												<IconLayout2Filled className="mt-" />
+												<IconLayout2Filled />
 											</div>
 											<div>
-												<p className="text-3xl text-zinc-600">
-													Edit Sesukamu
-												</p>
-												<p className=" text-zinc-400">
-													Unlimited Revisi
-												</p>
+												<p className="text-3xl text-zinc-600">Edit Sesukamu</p>
+												<p className="text-zinc-400">Unlimited Revisi</p>
 											</div>
 										</div>
 									</CardTitle>
 								</Card>
 							</div>
 
-							<div className="w-full md:w-7/12">
-								<Card
-									className="rounded-xl border border-pink-200  shadow-none p-10"
-									style={{ background: "#FEF4F9" }}
-								>
+							<div className="w-full md:w-6/12">
+								<Card className="rounded-xl border border-pink-200 shadow-none p-10" style={{ background: "#FEF4F9" }}>
 									<CardTitle>
 										<div className="flex gap-5">
 											<div className="bg-white p-3 rounded-xl items-center">
-												<IconLayout2Filled className="mt-" />
+												<IconLayout2Filled />
 											</div>
 											<div>
-												<p className="text-3xl text-zinc-600">
-													100+
-												</p>
-												<p className=" text-zinc-400">
-													Tema Undangan
-												</p>
+												<p className="text-3xl text-zinc-600">100+</p>
+												<p className="text-zinc-400">Tema Undangan</p>
 											</div>
 										</div>
 									</CardTitle>
@@ -97,48 +69,30 @@ const WelcomePage = ({ }) => {
 						</div>
 					</div>
 
-					<div className="w-4/12">
-						<Card
-							className="rounded-xl border border-pink-500 p-5 h-[550px] flex flex-col justify-between items-center"
-							style={{ borderWidth: "1px", background: "#EB578B" }}
-						>
+					<div className="w-full md:w-4/12">
+						<Card className="rounded-xl border border-pink-500 p-5 h-[550px] flex flex-col justify-between items-center" style={{ background: "#EB578B" }}>
 							<CardTitle>
 								<Carousel className="w-full h-auto mx-10">
 									<CarouselContent className="flex justify-center">
 										<CarouselItem className="pl-2 md:pl-4">
-											<img
-												src={MyImg}
-												alt=""
-												className="w-[280px] rounded-md"
-											/>
+											<img src={MyImg} alt="" className="w-[280px] rounded-md" />
 										</CarouselItem>
 										<CarouselItem className="pl-2 md:pl-4">
-											<img
-												src={MyImg}
-												alt=""
-												className="w-[280px] rounded-md"
-											/>
+											<img src={MyImg} alt="" className="w-[280px] rounded-md" />
 										</CarouselItem>
 										<CarouselItem className="pl-2 md:pl-4">
-											<img
-												src={MyImg}
-												alt=""
-												className="w-[280px] rounded-md"
-											/>
+											<img src={MyImg} alt="" className="w-[280px] rounded-md" />
 										</CarouselItem>
 									</CarouselContent>
 								</Carousel>
 							</CardTitle>
-							<Button
-								className="rounded-full  bg-pink-100 flex gap-2 hover:bg-pink-200 text-pink-500"
-							>
+							<Button className="rounded-full bg-pink-100 flex gap-2 hover:bg-pink-200 text-pink-500">
 								Lihat Detail <IconArrowRight size={18} />
 							</Button>
 						</Card>
-
-
 					</div>
 				</div>
+
 
 
 				{/* Bagian Template */}
